6b3eefc4434130a87135f13acd96ae08d0335ea6
GuilhermeNexus <63418716+GuilhermeNexus@users.noreply.github.com>
2021-06-14 15:09:15 -0300

    cursoController fixed

--- demo/src/main/java/com/example/demo/model/cursoController.java
+++ demo/src/main/java/com/example/demo/model/cursoController.java
@@ -30,7 +30,7 @@ public class cursoController {
 	public Curso getCursoid(@PathVariable long id) {
 		return repository.getById(id);
 	}
-	@PostMapping("/cursoadicionar")
+	@PostMapping("/curso")
 	public Curso salvarcurso(@RequestBody Curso curso) {
 		return repository.save(curso);
 	}
